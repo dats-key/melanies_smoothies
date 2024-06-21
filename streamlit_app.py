ba3a44df944b1dbcbd13696c9680f8a7b8894d6c
dats-key <saijo.tatsuya.5369@members.co.jp>
2024-06-21 11:19:16 +0900

    Update streamlit_app.py
    アプリへの接続方法をPC版からスマホ版に修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-# Write directly to the app
+# アプリのタイトルを記載
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
     """Choose the fruits you want in your custom Smoothe!
@@ -15,7 +14,8 @@ name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be:", name_on_order)
 
 # fruit_optionsからFRUIT_NAME列をSELECT
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
