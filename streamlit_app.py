409cb400ce7ec242929cdff37ae37ef4aabd2003
dats-key <saijo.tatsuya.5369@members.co.jp>
2024-06-21 17:57:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ if ingredients_list:
         ingredients_string += fruits_chosen + ' '
 
     # search_on列がある対象のみに絞るクエリを実行
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruits_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruits_chosen,' is ', search_on, '.')
 
     # 対象フルーツに対してfruityviceへREST API呼び出し
         st.subheader(fruits_chosen + 'Nutrition Information')
