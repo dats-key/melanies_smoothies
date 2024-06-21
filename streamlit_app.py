93916cc980e5f7b9e46c4413fbc1026beb842837
dats-key <saijo.tatsuya.5369@members.co.jp>
2024-06-21 16:37:00 +0900

    Update streamlit_app.py
    スペルミスを修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ if ingredients_list:
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
     # 対象フルーツに対してfruityviceへREST API呼び出し
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        st.subheader(fruits_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
