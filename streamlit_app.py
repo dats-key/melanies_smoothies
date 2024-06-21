d9bcdb780ff91b54459f2b1d57defaf1be443353
dats-key <saijo.tatsuya.5369@members.co.jp>
2024-06-21 17:53:55 +0900

    Update streamlit_app.py
    クエリを追加

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,6 +41,11 @@ if ingredients_list:
     # クエリ実行用の変数にフルーツ名を入れる
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
+
+    # search_on列がある対象のみに絞るクエリを実行
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
     # 対象フルーツに対してfruityviceへREST API呼び出し
         st.subheader(fruits_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_chosen)
